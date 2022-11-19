0fec66cf8173904c683cca3db59c8a54a8f86c63
Chandra Shekhar <chandra.pandey@winning.com.au>
2022-11-19 14:03:40 +1100

    Atlas search

---
+++ mongo/MongoCRUDCSharpConsole/AtlasSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MongoCRUDCSharpConsole
+{
+	/*
+	 * Apache lucene seaarch algo
+	 *  
+	 * Relevance based search: By customer, default search
+	 * database search: By Developers
+	 * Score:
+	 * Weght to different fields
+	 * 
+	 * $search & compound
+	 */
+	public class AtlasSearch
+	{
+		public AtlasSearch()
+		{
+		}
+	}
+}
+
